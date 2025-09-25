520f84c6ff656ba1478f4cb40aeba58146bc7a6f
DESKTOP-TRQOUEU\Kavinda Piyumal <solangakavinda@gmail.com>
2025-09-26 00:20:35 +0530

    first comment

---
+++ index.php
@@ -0,0 +1,9 @@
+<?php
+// Simple PHP Hello World app
+
+// Display Hello World
+echo "<h1>Hello, World!</h1>";
+
+// Display current date & time
+echo "<p>Current time is: " . date("Y-m-d H:i:s") . "</p>";
+?>
